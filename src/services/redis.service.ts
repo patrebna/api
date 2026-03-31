3340947442bdf10cdb157fd25f4950558970b3f2
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2026-03-31 19:21:41 +0300

    feat: обновление версии и добавление сервиса Redis для кэширования

---
+++ src/services/redis.service.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import Redis from 'ioredis';
+import { readFileSync } from 'fs';
+
+class RedisService {
+  private readonly redis: Redis;
+  constructor() {
+    const password = process.env.REDIS_PASSWORD ?? '';
+    this.redis = new Redis({
+      host: 'redis',
+      port: 6379,
+      password,
+      tls: {
+        ca: readFileSync('certs/ca.pem'),
+        cert: readFileSync('certs/client-cert.pem'),
+        key: readFileSync('certs/client-key.pem'),
+        rejectUnauthorized: false,
+      },
+    });
+
+    this.redis.once('connect', () => {
+      console.log('Успешное подключение к хранилищу кэша.');
+    });
+
+    this.redis.on('error', console.error.bind(console, 'Ошибка подключения к хранилищу кэша:'));
+  }
+
+  getClient(): Redis {
+    return this.redis;
+  }
+  async sendNotificationToBot(key: string, data: string): Promise<void> {
+    await this.redis.rpush(key, data);
+  }
+}
+
+const cache = new RedisService();
+export default cache;
