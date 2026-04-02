8a6d6a97b65a93fcabcb74cdd38962e808ca2cb0
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2026-04-02 06:57:25 +0300

    fix: исправлено форматирование данных при отправке уведомления в bepaidHandler

--- src/controllers/bepaid.controller.ts
+++ src/controllers/bepaid.controller.ts
@@ -2,6 +2,6 @@ import { type Request, type Response } from 'express';
 import cache from 'services/redis.service';
 
 export async function bepaidHandler(req: Request, res: Response): Promise<void> {
-  await cache.sendNotificationToBot('bot_queue_payments', JSON.stringify({ data: req.body }));
+  await cache.sendNotificationToBot('bot_queue_payments', JSON.stringify(req.body));
   res.status(200).json({ status: 'ok' });
 }
